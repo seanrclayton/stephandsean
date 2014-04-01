4b27c13b2969ea015e0db05872c619130f79a51a
Sean Clayton <sclayton@homeaway.com>
2014-04-01 11:06:42 -0500

    adding con dep

--- Wedding/app/controllers/rsvp_controller.rb
+++ Wedding/app/controllers/rsvp_controller.rb
@@ -3,4 +3,11 @@ class RsvpController < ApplicationController
   def index
 
   end
+
+  def new
+
+    @name = params['rsvper']
+    object = RSVP.new(:name => @name, :age => 2)
+    object.save
+  end
 end
